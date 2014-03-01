75369c83b32247ac94d24cc786087bd182cc355d
Francisco Rojas <frojas@groupon.com>
2014-03-01 00:20:44 -0300

    Add anotation to the models

--- models/item.rb
+++ models/item.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  status      :string(255)
+#  description :text
+#  price       :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Item < ActiveRecord::Base
   has_many :images
 end
