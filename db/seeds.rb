1367e6b7e1a5114ecb4d4a8f2528e4b5b892bdd2
Francisco Rojas <frojas@groupon.com>
2014-03-01 18:08:50 -0300

    add seed data

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+i = Item.create(title: "Mac book Pro Retina Display 15'", price: 800000, description: "Casi nuevo poco uso")
+i.images.create(capture: 'imagen referencial', url: "http://images.apple.com/macbook-pro/design-retina/images/fluidgallery/gallery3_2256_2x.jpg")
+i.images.create(capture: 'imagen referencial', url: "http://images.apple.com/macbook-pro/design-retina/images/fluidgallery/gallery2_2256_2x.jpg")
+
+i = Item.create(title: "Bicicleta Merida MATTS 10-V 2012", price: 100000, description: "pagina con descipción del producto http://www.full-bike.cl/verproducto.php?producto=1074")
+i.images.create(capture: 'imagen referencial', url: "http://www.full-bike.cl/tmp_img/1074.jpg")
+
+i = Item.create(title: "Apple Thunderbolt Display", price: 500000, description: "pagina con descipción del produto http://www.apple.com/displays/")
+i.images.create(capture: 'imagen referencial', url: "http://images.apple.com/displays/images/bigpicture_2x.jpg")
+
+
