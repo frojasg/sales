88d44daac1f408726c17e51f64bc9a03205bbae2
Francisco Rojas <frojas@groupon.com>
2014-03-02 22:25:29 -0300

    refactor main controller using underscorejs

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,21 +1,13 @@
 'use strict';
 
 angular.module('salesApp')
-  .controller('MainCtrl', function (Itemservice, $scope) {
+  .controller('MainCtrl', function (Itemservice, $scope, _) {
     Itemservice.items().then(function(items) {
       $scope.items = items;
       var index = 0;
-      $scope.splitted = [];
-      $scope.splitted[index] = [];
-      for(var i = 0; i < $scope.items.length; i++) {
-        var item = $scope.items[i];
-        $scope.splitted[index].push(item);
-        if(i!== 0 && i % 2 === 0) {
-          index++;
-          $scope.splitted[index] = [];
-        }
-      }
+      $scope.splitted =  _.groupBy(items, function(item, i) {
+        return Math.floor(i/3);
+      });
     });
     $scope.default_image = "https://mashedmusings.files.wordpress.com/2012/03/christmas-shopping-for-women-3.jpg";
-
   });
