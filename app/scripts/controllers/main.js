5635181175b2c5188a06e1b9f9fe2c73c6532f30
Francisco Rojas <frojas@alumnos.utalca.cl>
2014-05-07 23:04:38 -0400

    Move logic from the view to the controller
    I'm getting an 404 error because i'm trying to access to an image that
    doesn't exist. I hoped this change addessed this issue, but it doesn't.
    Looks like the problems come when the items array is empty. I wonder if
    hidden the block when the items is empty would fix the problem.
    
    For the bright side i was able to remove logic form the view, but i'm
    not totally conformable with this change. I would like to map the item
    to a local class that contains extra presentation logic

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,21 @@
 
 angular.module('salesApp')
   .controller('MainCtrl', function (ItemService, $scope, _) {
+    $scope.default_image = "https://mashedmusings.files.wordpress.com/2012/03/christmas-shopping-for-women-3.jpg";
+
     ItemService.items().then(function(items) {
-      $scope.items = items;
+      $scope.items = _.map(items, function(item) {
+        item.image = function() {
+          if(item.images.length > 0) {
+            return item.images[0].url;
+          } else {
+            return $scope.default_image;
+          }
+        };
+        return item;
+      });
       $scope.splitted =  _.groupBy(items, function(item, i) {
         return Math.floor(i/3);
       });
     });
-    $scope.default_image = "https://mashedmusings.files.wordpress.com/2012/03/christmas-shopping-for-women-3.jpg";
   });
