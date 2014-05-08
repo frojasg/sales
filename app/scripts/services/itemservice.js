ba56245ed5a7e0345c7d02637c202acc1556c606
Francisco Rojas <frojas@alumnos.utalca.cl>
2014-05-07 23:29:14 -0400

    remove debug statement

--- app/scripts/services/itemservice.js
+++ app/scripts/services/itemservice.js
@@ -4,7 +4,6 @@ angular.module('salesApp')
   .service('ItemService', function ItemService($http) {
     this.items = function() {
       var promise = $http.get('/items').then(function(response) {
-        console.log(response);
         return response.data;
       });
       return promise;
