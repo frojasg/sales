7995775393cf329f85c6668aa29bca4b497e8812
Francisco Rojas <frojas@alumnos.utalca.cl>
2014-06-27 00:27:47 -0400

    Add facebook client

---
+++ services/facebook.rb
@@ -0,0 +1,8 @@
+class Facebook
+  def self.me(access_token)
+    response = Typhoeus.get("https://graph.facebook.com/me?access_token=#{access_token}")
+    body = JSON.parse(response.body, symbolize_names: true)
+    raise ArgumentError, body[:error][:message] unless response.code == 200
+    body.slice(:username, :first_name, :last_name, :id)
+  end
+end
---
+++ spec/services/facebook_spec.rb
@@ -0,0 +1,30 @@
+require 'web_spec_helper'
+require_relative '../../services/facebook'
+
+require 'json'
+require 'typhoeus'
+require 'active_support/core_ext/hash'
+
+describe Facebook do
+
+  context 'when we get a valid access token' do
+    let(:access_token) {'CAALuFtY5lz4BABBY3bjNK8v4uZCxRrcGuzFESshbb8pvBJY37IeRZBMRykKW6jX7Bc3t9DFDVC1qSoWzKc2xOh8zrU1I3cwMAZCvsrN9zrH7yUiwjftFcil3ZAw2JfcRHBcFRZB6dzRKDbQUiPCfAc6k8StJWZAQgUbonPcXsvg39XxRLZAiDg1NwjxqtfwsoAGgvnWGW4GhwZDZD'}
+
+    it 'fetch user profile information from facebook' do
+      VCR.use_cassette 'profile' do
+        user = Facebook.me(access_token)
+        expect(user).to eq({username: 'frojasg', first_name: 'Francisco', last_name: 'Rojas', id: '712817506'})
+      end
+    end
+  end
+
+  context 'when we get an invalid access_token' do
+    let(:access_token) {'invalid'}
+
+    it 'raise an error' do
+      VCR.use_cassette 'invalid_acces_token' do
+        expect{Facebook.me(access_token)}.to raise_error(ArgumentError, "Invalid OAuth access token.")
+      end
+    end
+  end
+end
