2e6451734e6d5d461ce79b3148b62c05bcc7efed
Francisco Rojas <frojas@alumnos.utalca.cl>
2014-07-17 22:20:30 -0400

    add spec for UserService.fetch

--- test/spec/services/userservice.js
+++ test/spec/services/userservice.js
@@ -7,12 +7,52 @@ describe('Service: Userservice', function () {
 
   // instantiate service
   var UserService;
-  beforeEach(inject(function (_UserService_) {
+  var $httpBackend;
+  var Facebook;
+  var $rootScope;
+
+  beforeEach(inject(function (_UserService_, _$httpBackend_, _Facebook_, _$rootScope_) {
     UserService = _UserService_;
+    $httpBackend = _$httpBackend_;
+    Facebook = _Facebook_;
+    $rootScope = _$rootScope_;
   }));
 
   it('should do something', function () {
     expect(!!UserService).toBe(true);
   });
 
+  describe('fetchUser', function() {
+    beforeEach(function() {
+      spyOn(Facebook, 'getLoginStatus');
+    });
+
+    it('We use facebook function', function() {
+      var result = UserService.fetchUser();
+      expect(Facebook.getLoginStatus).toHaveBeenCalled();
+      expect(Facebook.getLoginStatus.calls.length).toEqual(1);
+    });
+
+    describe('when callback is used', function() {
+      beforeEach(function() {
+        this.result = UserService.fetchUser();
+        this.f = Facebook.getLoginStatus.mostRecentCall.args[0];
+        this.onComplete = jasmine.createSpy('onComplete');
+      });
+
+      it('the user gave access to our app', function() {
+        this.f({status: 'connected', authResponse: {accessToken: '123'}});
+        this.result.then(this.onComplete);
+        $rootScope.$digest();
+        expect(this.onComplete).toHaveBeenCalledWith({status: 'connected', authResponse: {accessToken: '123'}});
+      });
+
+      it('the user gave not access to our app', function() {
+        this.f({status: 'rejected'})
+        this.result.then(function(){}, this.onComplete);
+        $rootScope.$digest();
+        expect(this.onComplete).toHaveBeenCalled();
+      });
+    });
+  });
 });
