7caffb83ad4372a6c5d8afbb6c119d2545a80340
Francisco Rojas <frojas@alumnos.utalca.cl>
2014-07-26 01:16:47 -0400

    Refactor header into a directive
    Instad of use create a new controller an import a template we use a
    angular directive. It's a small change but looks way better

--- app/scripts/users/user.js
+++ app/scripts/users/user.js
@@ -8,7 +8,7 @@ app.directive('userAlerts', function() {
     templateUrl: 'scripts/users/user-alerts.html',
     controller: function($scope) {
       this.alert = false;
-      self = this;
+      var self = this;
       this.closeAlert = function() {
         this.alert = false;
       };
@@ -17,5 +17,52 @@ app.directive('userAlerts', function() {
       });
     },
     controllerAs: 'alerts'
+  };
+});
+
+app.directive('navBar', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'scripts/users/navbar.html',
+    controller: function($scope, Facebook, UserService, $timeout) {
+      $scope.user = UserService.getUser();
+      $scope.userService = UserService;
+      $scope.logged = UserService.isLogged();
+      $scope.alert = false;
+      this.scope = $scope;
+      var self = this;
+
+      $scope.$on('user.logged', function(event, user) {
+        self.scope.saludation = true;
+        self.scope.user = user;
+        self.scope.logged = true;
+      });
+
+      $scope.$on('user.logout', function() {
+        self.scope.byebye = true;
+        self.scope.user = null;
+        self.scope.logged = false;
+        $timeout(function() {
+          self.scope.byebye = false;
+        }, 2000);
+      });
+
+      $scope.$on('user.clean', function() {
+        self.scope.user = null;
+        self.scope.logged = false;
+      });
+
+      $scope.$watch(
+        function() {
+          return Facebook.isReady();
+        },
+        function(isReady) {
+          $scope.facebookReady = isReady;
+        }
+      );
+      $scope.login = UserService.login;
+      $scope.logout = UserService.logout;
+    },
+    controllerAs: 'nav'
   };
 });
